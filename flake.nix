9530a349e3f808a8a4faa32cc5dc79995e1d0e16
SnO2WMaN <me@sno2wman.net>
2022-08-15 18:21:39 +0900

    build with nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    npmlock2nix = {
+      url = "github:nix-community/npmlock2nix";
+      flake = false;
+    };
     devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
 
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -13,6 +17,7 @@
   outputs = {
     self,
     nixpkgs,
+    npmlock2nix,
     flake-utils,
     devshell,
     ...
@@ -23,9 +28,22 @@
           inherit system;
           overlays = [
             devshell.overlay
+            (self: super: {
+              npmlock2nix = super.callPackage npmlock2nix {};
+            })
           ];
         };
       in {
+        packages.website = pkgs.npmlock2nix.build {
+          src = ./.;
+          installPhase = "cp -r dist $out";
+          buildCommands = [
+            "npm run build"
+          ];
+        };
+        packages.default = self.packages.${system}.website;
+        defaultPackage = self.packages.${system}.default;
+
         devShells.default = pkgs.devshell.mkShell {
           imports = [
             (pkgs.devshell.importTOML ./devshell.toml)
