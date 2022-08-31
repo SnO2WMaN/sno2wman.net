c384d477f9be0f2691ea3a42765dbf8e65d10862
SnO2WMaN <me@sno2wman.net>
2022-08-31 20:33:23 +0900

    update environments

--- flake.nix
+++ flake.nix
@@ -1,12 +1,21 @@
 {
+  # main
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     npmlock2nix = {
       url = "github:nix-community/npmlock2nix";
       flake = false;
     };
-    devshell.url = "github:numtide/devshell";
+    # dhall-haskell = {
+    #   url = "github:dhall-lang/dhall-haskell/1.41.2";
+    #   flake = false;
+    # };
+  };
 
+  # dev
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    nix-filter.url = "github:numtide/nix-filter";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -17,34 +26,48 @@
   outputs = {
     self,
     nixpkgs,
-    npmlock2nix,
-    flake-utils,
     devshell,
+    flake-utils,
+    nix-filter,
     ...
-  }:
+  } @ inputs:
     flake-utils.lib.eachDefaultSystem (
       system: let
+        # dhall-haskell' = (import "${inputs.dhall-haskell}/nix/shared.nix") {
+        #   inherit system nixpkgs;
+        # };
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
             devshell.overlay
-            (self: super: {
-              npmlock2nix = super.callPackage npmlock2nix {};
-            })
+            (
+              final: prev: {
+                # inherit
+                #   (dhall-haskell')
+                #   dhall-json
+                #   dhall-lsp-server
+                #   ;
+                npmlock2nix = final.callPackage inputs.npmlock2nix {};
+              }
+            )
           ];
         };
         inherit (pkgs) lib;
       in {
+        # packages.dhall-haskell = import inputs.dhall-haskell;
         packages.website = pkgs.npmlock2nix.build {
-          src = builtins.path {
-            path = ./.;
-            filter = name: type:
-              (name == toString ./package.json)
-              || (name == toString ./package-lock.json)
-              || (name == toString ./.tsconfig.json)
-              || (name == toString ./.browserslistrc)
-              || (lib.hasPrefix (toString ./src) name);
-          };
+          src = with nix-filter.lib;
+            filter {
+              root = ./.;
+              include = [
+                "./package.json"
+                "./package-lock.json"
+                "./tsconfig.json"
+                "./.browserslistrc"
+                (inDirectory "src")
+              ];
+              # exclude = [];
+            };
           installPhase = "cp -r dist $out";
           buildCommands = [
             "npm run build"
@@ -59,8 +82,24 @@
         apps.default = self.apps.${system}.preview;
 
         devShells.default = pkgs.devshell.mkShell {
-          imports = [
-            (pkgs.devshell.importTOML ./devshell.toml)
+          packages = with pkgs; [
+            alejandra
+            dprint
+            nodejs-16_x
+            treefmt
+            dhall-lsp-server
+          ];
+          commands = [
+            {
+              package = "treefmt";
+              category = "formatters";
+            }
+          ];
+          env = [
+            {
+              name = "PATH";
+              eval = "$PATH:$PRJ_ROOT/node_modules/.bin";
+            }
           ];
         };
       }
