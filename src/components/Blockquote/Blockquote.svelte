6b1fcc6ce90fd72f461be476b651d53453495f29
SnO2WMaN <me@sno2wman.net>
2022-10-27 03:24:37 +0900

    lightmode

--- src/components/Blockquote/Blockquote.svelte
+++ src/components/Blockquote/Blockquote.svelte
@@ -5,6 +5,12 @@
   let quote = quotes[Math.floor(Math.random() * quotes.length)];
 </script>
 
-<blockquote class={clsx(["text-sm"], ["italic"], ["text-slate-400"])}>
+<blockquote
+  class={clsx(
+    ["text-sm"],
+    ["italic"],
+    ["text-slate-800", "dark:text-slate-400"]
+  )}
+>
   {quote.text}
 </blockquote>
