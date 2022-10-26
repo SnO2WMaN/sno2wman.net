7bb84b5525441cfaf658a77ffd37b5f78b6f09f1
SnO2WMaN <me@sno2wman.net>
2022-10-26 20:41:16 +0900

    astro wip

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   # main
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    corepack.url = "github:SnO2WMaN/corepack-flake";
+
     npmlock2nix = {
       url = "github:nix-community/npmlock2nix";
       flake = false;
@@ -15,7 +17,6 @@
   # dev
   inputs = {
     devshell.url = "github:numtide/devshell";
-    nix-filter.url = "github:numtide/nix-filter";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -39,53 +40,21 @@
           inherit system;
           overlays = with inputs; [
             devshell.overlay
-            nix-filter.overlays.default
-            (
-              final: prev: {
-                # inherit
-                #   (dhall-haskell')
-                #   dhall-json
-                #   dhall-lsp-server
-                #   ;
-                npmlock2nix = final.callPackage inputs.npmlock2nix {};
-              }
-            )
+            corepack.overlays.default
           ];
         };
         inherit (pkgs) lib;
       in {
-        # packages.dhall-haskell = import inputs.dhall-haskell;
-        packages.website = pkgs.callPackage ./website {};
-        packages.default = self.packages.${system}.website;
-        defaultPackage = self.packages.${system}.default;
-
-        apps.preview = flake-utils.lib.mkApp {
-          drv = with pkgs; (
-            writeShellScriptBin
-            "serve-preview"
-            "${miniserve}/bin/miniserve ${self.packages.${system}.website}"
-          );
-        };
-        apps.default = self.apps.${system}.preview;
-
         devShells.default = pkgs.devshell.mkShell {
           packages = with pkgs; [
             alejandra
             dprint
-            nodejs-16_x
             treefmt
-            dhall
-            dhall-json
-            dhall-lsp-server
-            deno
-            taplo-cli
-            cargo-make
-          ];
-          commands = [
-            {
-              package = "treefmt";
-              category = "formatters";
-            }
+            nodejs-16_x
+            (mkCorepack {
+              nodejs = nodejs-16_x;
+              pm = "pnpm";
+            })
           ];
         };
       }
