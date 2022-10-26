6963eafd412234b462faee31ced9b83eb4a00650
SnO2WMaN <me@sno2wman.net>
2022-10-27 05:57:57 +0900

    Quote

---
+++ src/components/Quote/Quote.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import clsx from "clsx";
+  import quotes from "./quotes.json";
+
+  let quote = quotes[Math.floor(Math.random() * quotes.length)];
+</script>
+
+<figure>
+  <blockquote
+    class={clsx(
+      ["text-sm"],
+      ["italic"],
+      ["text-slate-800", "dark:text-slate-400"]
+    )}
+    cite={quote.link}
+  >
+    {quote.text}
+  </blockquote>
+  <figcaption
+    class={clsx(["text-xs"], ["text-slate-600", "dark:text-slate-500"])}
+  >
+    —<a href={quote.link}>{quote.reference}</a>
+  </figcaption>
+</figure>
+
+<style>
+  figure {
+    animation: blur 0.5s ease-in-out 0.25s both;
+  }
+
+  @keyframes blur {
+    from {
+      opacity: 0;
+      transform: translateX(-8px);
+    }
+    to {
+      opacity: 1;
+    }
+  }
+</style>
