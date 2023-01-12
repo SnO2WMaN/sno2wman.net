b04e734cf597644ae514e1be9273734501b7c7b1
SnO2WMaN <me@sno2wman.net>
2023-01-12 16:33:21 +0900

    main animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  integrations: [svelte()],
 });
