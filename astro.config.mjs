d2a9c6f950525befec0bab439b08114ee643ad2a
SnO₂WMaN <me@sno2wman.net>
2023-02-28 15:24:31 +0900

    SSRサポート (#13)
    * svelte
    
    * add wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
+import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+  output: "server",
+  adapter: cloudflare()
 });
