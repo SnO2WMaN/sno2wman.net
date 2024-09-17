c3a478ad08251ac850faed788f9c8949668162ac
SnO2WMaN <me@sno2wman.net>
2024-09-18 03:33:58 +0900

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), tailwind()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
