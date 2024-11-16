6b87e4a44483af60938b744a54f15484d8496eea
SnO2WMaN <me@sno2wman.net>
2024-11-16 18:34:44 +0900

    fx

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), tailwind()],
-	output: "server",
-	adapter: cloudflare(),
-	redirects: {
-		"/projects": "/#projects",
-		"/links": "/social",
-		"/aboutme": "/",
-	},
+  integrations: [svelte(), tailwind()],
+  output: "server",
+  adapter: cloudflare(),
+  redirects: {
+    "/projects": "/#projects",
+    "/links": "/social",
+    "/aboutme": "/",
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				serif: ["BIZ UDPMincho", "sans-serif"],
-				sans: ["Shippori Antique B1", "serif"],
-				mono: ["JetBrains Mono Variable", "monospace"],
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["BIZ UDPMincho", "sans-serif"],
+        sans: ["Shippori Antique B1", "serif"],
+        mono: ["JetBrains Mono Variable", "monospace"],
+      },
+    },
+  },
+  plugins: [],
 };
