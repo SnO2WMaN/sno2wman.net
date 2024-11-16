2efd0f627beecb20cdef5ca8882f6b910632b38f
SnO2WMaN <me@sno2wman.net>
2024-11-16 16:46:33 +0900

    home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
-      fontFamily: {
-        "serif": ["BIZ UDPMincho", 'sans-serif'],
-        "sans": ['Shippori Antique B1', 'serif'],
-        "crimson-pro" : ['Crimson Pro Display Variable', 'serif'],
-        "red-hat-display": ['Red Hat Display Variable', 'sans-serif'],
-      },
-    },
+			fontFamily: {
+				serif: ["BIZ UDPMincho", "sans-serif"],
+				sans: ["Shippori Antique B1", "serif"],
+				mono: ["JetBrains Mono Variable", "monospace"],
+			},
+		},
 	},
 	plugins: [],
-}
+};
