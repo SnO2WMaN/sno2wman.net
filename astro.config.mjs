ba0535f231dddb1c947d9d47a8d8dfa07bb559d7
SnO₂WMaN <me@sno2wman.net>
2024-11-16 18:37:02 +0900

    Recreate (#709)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), tailwind()],
   output: "server",
   adapter: cloudflare(),
+  redirects: {
+    "/projects": "/#projects",
+    "/links": "/social",
+    "/aboutme": "/",
+  },
 });
